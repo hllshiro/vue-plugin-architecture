363d81d0d41f649220562c120b61cc6ea40616a7
hllshiro <40970081+hllshiro@users.noreply.github.com>
2025-10-20 17:29:24 +0800

    refactor(pluginEventApi): simplify scope handling by using getScope method

--- packages/core/src/proxy/pluginEventApi.ts
+++ packages/core/src/proxy/pluginEventApi.ts
@@ -11,19 +11,23 @@ export class PluginEventApi implements IPluginEventAPI {
     private readonly eventBus: IEventBus
   ) {}
 
+  private getScope(scope?: string) {
+    return scope ?? this.name
+  }
+
   on<Key extends keyof PluginEvents>(
     event: Key,
     handler: Handler<PluginEvents[Key]>,
     scope?: string
   ): void {
-    this.eventBus.on(event, handler, scope ?? this.name)
+    this.eventBus.on(event, handler, this.getScope(scope))
   }
   off<Key extends keyof PluginEvents>(
     event: Key,
     handler?: Handler<PluginEvents[Key]>,
     scope?: string
   ): void {
-    this.eventBus.off(event, handler, scope ?? this.name)
+    this.eventBus.off(event, handler, this.getScope(scope))
   }
   emit<Key extends keyof PluginEvents>(
     event: Key,
@@ -39,13 +43,17 @@ export class PluginEventApi implements IPluginEventAPI {
     payload?: PluginEvents[Key],
     scope?: string
   ): void {
-    this.eventBus.emit(event, payload as PluginEvents[Key], scope ?? this.name)
+    this.eventBus.emit(
+      event,
+      payload as PluginEvents[Key],
+      this.getScope(scope)
+    )
   }
   once<Key extends keyof PluginEvents>(
     event: Key,
     handler: Handler<PluginEvents[Key]>,
     scope?: string
   ): void {
-    this.eventBus.once(event, handler, scope ?? this.name)
+    this.eventBus.once(event, handler, this.getScope(scope))
   }
 }
